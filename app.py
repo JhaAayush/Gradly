dd945e221012fa2ad60e53ad1fa3e7ddc0c03674
aayush <aayushjha4901@gmail.com>
2025-11-14 01:12:20 +0530

    Messaging Complete v4

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def home():
     return render_template('home.html', closest_task=None)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
